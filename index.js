dcbc223c9a93f2c5792f1ae566a7e75b7e454693
Faris Han <farishanugraha@gmail.com>
2021-06-26 19:24:28 +0700

    Add built-in ID generator

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function Player({
   name
 }){
-  this.id = `Player_${ID()}`
+  this.id = `Player${ID()}`
   this.name = name
   this.created_at = Date.now()
 
   this.characters = []
 
   return this
+
+  // Helpers
+  function ID() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
 }
