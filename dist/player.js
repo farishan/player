0050ab25b24a9d774f66343ce2abd873343378e1
Faris Han <farishanugraha@gmail.com>
2021-06-26 19:29:19 +0700

    Add npm scripts

---
+++ dist/player.js
@@ -0,0 +1,1 @@
+function Player({name}){this.id=`Player${ID()}`;this.name=name;this.created_at=Date.now();this.characters=[];return this;function ID(){return"_"+Math.random().toString(36).substr(2,9)}}
