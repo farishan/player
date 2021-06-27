13bcbe67df3348902d7d6937be325ad8e1abd4f6
Faris Han <farishanugraha@gmail.com>
2021-06-27 18:27:41 +0700

    Add default player name

--- index.js
+++ index.js
@@ -4,28 +4,25 @@
  * @param {Object} extend - Additional properties
  * @returns Player object
  */
-function Player({
-  name,
-  extend = {},
-}){
-  this.id = `Player${ID()}`
-  this.name = name
-  this.created_at = Date.now()
-  this.characters = []
+function Player({ name = "default", extend = {} }) {
+  this.id = `Player${ID()}`;
+  this.name = name;
+  this.created_at = Date.now();
+  this.characters = [];
 
   // Feature-specific properties
-  const extendKeys = Object.keys(extend)
-  if(extendKeys.length > 0){
+  const extendKeys = Object.keys(extend);
+  if (extendKeys.length > 0) {
     for (let index = 0; index < extendKeys.length; index++) {
       const element = extendKeys[index];
-      this[element] = extend[element]
+      this[element] = extend[element];
     }
   }
 
-  return this
+  return this;
 
   // Helpers
   function ID() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-  };
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
 }
